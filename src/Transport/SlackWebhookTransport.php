950686243be4b974ffd009f6fd526dc65f9bdf87
Daniel STANCU <daniel.stancu@me.com>
2020-03-17 23:56:42 +0200

    Permit null user icon / emoji

--- src/Transport/SlackWebhookTransport.php
+++ src/Transport/SlackWebhookTransport.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Symfony package.
  *
- * (c) Fabien Potencier <fabien@symfony.com>
+ * (c) Daniel Stancu <birkof@birkof.ro>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -43,7 +43,7 @@ final class SlackWebhookTransport extends AbstractTransport
         string $webhookPath,
         string $channel,
         string $username,
-        string $userIcon,
+        string $userIcon = null,
         HttpClientInterface $client = null,
         EventDispatcherInterface $dispatcher = null
     ) {
